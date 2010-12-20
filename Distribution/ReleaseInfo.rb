3b8922dabc6c7c765ee72a921ae1a047dc8b9e78
Muriel Salvan <MurielSalvan@users.sourceforge.net>
2010-12-20 16:30:14 +0000

    * Released version 0.1.0.20101220. * Renamed example config file.
    git-svn-id: https://statscollect.svn.sourceforge.net/svnroot/statscollect@2 267631ea-fd71-45b9-892a-3e1a2555dbbe

---
+++ Distribution/ReleaseInfo.rb
@@ -0,0 +1,53 @@
+#--
+# Copyright (c) 2009-2010 Muriel Salvan (murielsalvan@users.sourceforge.net)
+# Licensed under the terms specified in LICENSE file. No warranty is provided.
+#++
+
+RubyPackager::ReleaseInfo.new.
+  author(
+    :Name => 'Muriel Salvan',
+    :EMail => 'murielsalvan@users.sourceforge.net',
+    :WebPageURL => 'http://murielsalvan.users.sourceforge.net'
+  ).
+  project(
+    :Name => 'StatsCollect',
+    :WebPageURL => 'http://statscollect.sourceforge.net/',
+    :Summary => 'Command line tool gathering statistics from external sources.',
+    :Description => 'StatsCollect is a little framework gathering statistics from external sources (social networks, web sites...), stored in pluggable backends. It can be very easily extended thanks to its plugins (currently include Facebook, Myspace, Youtube, Google).',
+    :ImageURL => 'http://statscollect.sourceforge.net/wiki/images/c/c9/Logo.jpg',
+    :FaviconURL => 'http://statscollect.sourceforge.net/wiki/images/2/26/Favicon.png',
+    :SVNBrowseURL => 'http://statscollect.svn.sourceforge.net/viewvc/statscollect/',
+    :DevStatus => 'Beta'
+  ).
+  addCoreFiles( [
+    '{lib,bin}/**/*'
+  ] ).
+#  addTestFiles( [
+#    'test/**/*'
+#  ] ).
+  addAdditionalFiles( [
+    'README',
+    'LICENSE',
+    'AUTHORS',
+    'Credits',
+    'TODO',
+    'ChangeLog',
+    '*.example'
+  ] ).
+  gem(
+    :GemName => 'StatsCollect',
+    :GemPlatformClassName => 'Gem::Platform::RUBY',
+    :RequirePath => 'lib',
+    :HasRDoc => true
+#    :TestFile => 'test/run.rb'
+  ).
+  sourceForge(
+    :Login => 'murielsalvan',
+    :ProjectUnixName => 'statscollect'
+  ).
+  rubyForge(
+    :ProjectUnixName => 'statscollect'
+  ).
+  executable(
+    :StartupRBFile => 'bin/StatsCollect.rb'
+  )
